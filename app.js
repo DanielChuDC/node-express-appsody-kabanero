b90d642a85ef6ecd96193523006ac3ee6fb0edec
JiaHao-Chu <JiaHao.Chu@ibm.com>
2019-11-07 13:28:26 +0800

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kabanero IBM Cloud Pak!");
+  res.send("Hello from Kabanero IBM Cloud Pak Application!");
 });
  
 module.exports.app = app;
