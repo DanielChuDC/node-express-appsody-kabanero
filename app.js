ad0b65418b80e7637a31623c9e7f2c5cccf1118a
JiaHao-Chu <JiaHao.Chu@ibm.com>
2019-11-08 21:08:36 +0800

    1011

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kabanero IBM Cloud Pak Application 101!");
+  res.send("Hello from Kabanero IBM Cloud Pak Application 1011!");
 });
  
 module.exports.app = app;
